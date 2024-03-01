bfd4362fa51fa30e3b0f21783774ff1404e8591c
xyun <xyduane@qq.com>
2024-03-01 20:57:16 +0800

    Update addBasicAuth.js

--- addBasicAuth.js
+++ addBasicAuth.js
@@ -1,3 +1,16 @@
+
+/******************************
+
+脚本功能：增加认证用户
+
+*******************************
+[mitm]
+hostname = www.rtfund.com,wx.rtfund.com
+
+[rewrite_local]
+^https://wx.rtfund.com/gbusweb/msitepro/$ url script-response-body https://raw.githubusercontent.com/xyduane/apptest/main/addBasicAuth.js
+*******************************/
+
 var modifiedHeaders = $request.headers;
 modifiedHeaders['Authorization'] = 'Basic eHl1bjoxMTEx';
 
