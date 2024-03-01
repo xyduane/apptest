ec9b94c63f6099efbb19deb7f631158a470f6bcc
xyun <xyduane@qq.com>
2024-03-01 19:48:26 +0800

    Update pageSpy.js

--- pageSpy.js
+++ pageSpy.js
@@ -7,7 +7,7 @@
 hostname = wx.rtfund.com
 
 [rewrite_local]
-^https:\/\/wx\.rtfund\.com\/gbusweb\/msitepro url script-request-body https://raw.githubusercontent.com/xyduane/apptest/main/pageSpy.js
+^https:\/\/wx\.rtfund\.com url script-request-body https://raw.githubusercontent.com/xyduane/apptest/main/pageSpy.js
 *******************************/
 
 console.log($response)
