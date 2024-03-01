d52cbdded1549b7dda896ee0f20ee66a77a0ec4c
xyun <xyduane@qq.com>
2024-03-01 17:05:14 +0800

    Update pageSpy.js

--- pageSpy.js
+++ pageSpy.js
@@ -1,1 +1,15 @@
+var body = $response.body;
+var url = $request.url;
 
+if (body) {
+  if(url=='https://wx.rtfund.com/gbusweb/msitepro/'){
+    body = body.replace('</head>',`<script crossorigin="anonymous" src="https://warp.xylucky.top/page-spy/index.min.js"></script>
+<script>
+  window.$pageSpy = new PageSpy();
+</script>
+</head>`)
+  }
+  $done({ body: body });
+} else {
+  $done({})
+}
